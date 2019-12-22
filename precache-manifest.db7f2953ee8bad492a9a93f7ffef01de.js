68c3041cca7406fc6e194e1b0028cfc5ee98cf40
TomBecker1987 <figleggedtom87@gmail.com>
2019-12-22 22:57:05 +0200

    Updates

---
+++ precache-manifest.db7f2953ee8bad492a9a93f7ffef01de.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e47285db5929a3b29aed00a8019895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1a969d58def17cf723e",
+    "url": "/static/css/main.8cceb232.chunk.css"
+  },
+  {
+    "revision": "4045485b0dea5d3a9be6",
+    "url": "/static/js/2.b0ead3ba.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.b0ead3ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1a969d58def17cf723e",
+    "url": "/static/js/main.d94fe43b.chunk.js"
+  },
+  {
+    "revision": "32f2f5113c7f9417e150",
+    "url": "/static/js/runtime-main.ef25631f.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/static/media/15-s.39ac499c.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/static/media/16-s.0cf9cad1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/static/media/17-s.3c85da3e.png"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/static/media/41-s.421c13f1.png"
+  },
+  {
+    "revision": "3d543d03f557945240c978ed45d15f95",
+    "url": "/static/media/clouds.3d543d03.jpg"
+  }
+]);
